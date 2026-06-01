c44b72df89e1da34292c730d84596c1cd66b099d
Gunasheelan24 <gunasheelan16@gmail.com>
2026-06-01 21:17:45 +0530

    Redux: added the custom hooks for useSelector & useDispatch Method

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@ import App from "./App.tsx";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import store from "./store/Redux/Store.ts";
+// import store from "./store/Redux/Store.ts";
+import { rtkStore } from "./store/Redux-Toolkit/Store.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider store={store}>
+    <Provider store={rtkStore}>
       <App />
     </Provider>
   </StrictMode>,
--- src/store/Redux-Toolkit/Store.tsx
+++ src/store/Redux-Toolkit/Store.tsx
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./AmountSlice";
 
-export const store = configureStore({
+export const rtkStore = configureStore({
   reducer: {
     bankAmount: reducer,
   },
 });
+
+// Types
+export type RootState = ReturnType<typeof rtkStore.getState>;
+export type AppDispatch = typeof rtkStore.dispatch;
